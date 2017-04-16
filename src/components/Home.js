320e49f073051234e682a08502f7929deed89b5e
DaxChen <dd@daxchen.com>
2017-04-16 17:57:14 +0800

    add some descriptions and github code link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
 export default () => (
-  <h1>
-    You are now at HOME PAGE (<code>route: {'/'}</code>)
-  </h1>
+  <div>
+    <h1>
+      You are now at HOME PAGE (<code>route: {'/'}</code>)
+    </h1>
+    <br />
+    <p>
+      This example shows how you can use material-ui's <code>MenuItem</code> together with react-router's <code>Link</code>
+    </p>
+    <br />
+    <p>
+      See {' '}
+      <a target="_blank" href="https://github.com/DaxChen/material-ui-Link-within-MenuItem/blob/master/src/App.js#L35-L52">
+        these lines of code 
+      </a>
+      {' '} on how this works.
+    </p>
+    <br />
+    <p>
+      Open the Drawer and click on links to see them in action.
+    </p>
+  </div>
 )
